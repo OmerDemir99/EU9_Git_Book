ac03499e3cefed8847313b0c9fe91ede872439a0
OmerDemir <samiderin11@gmail.com>
2022-07-30 12:24:51 +0300

    deleted first class

--- src/chapter1/FirstClass.java
+++
@@ -1,8 +0,0 @@
-package chapter1;
-
-public class FirstClass {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("Hello again");
-    }
-}
