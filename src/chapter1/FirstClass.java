5f4319540044975ca49501dc014ce342ad966dec
OmerDemir <samiderin11@gmail.com>
2022-07-30 11:58:48 +0300

    created first class

--- src/chapter1/FirstClass.java
+++ src/chapter1/FirstClass.java
@@ -3,5 +3,6 @@ package chapter1;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello again");
     }
 }
